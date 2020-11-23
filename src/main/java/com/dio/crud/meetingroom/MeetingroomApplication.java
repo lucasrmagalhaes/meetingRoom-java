aa3f12053c71b578095e945c00e6ea75e4a9be41
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-11-22 23:29:14 -0300

    definindo a model

---
+++ src/main/java/com/dio/crud/meetingroom/MeetingroomApplication.java
@@ -0,0 +1,13 @@
+package com.dio.crud.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/crud/meetingroom/MeetingroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.crud.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
